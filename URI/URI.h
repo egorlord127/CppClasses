49fe1126d87d91fcc58b5edb8cbb61d059412afb
Egor Lord <egor.lord-127@yandex.com>
2017-08-17 10:33:34 -0700

    URI encode&decode

---
+++ URI/URI.h
@@ -0,0 +1,7 @@
+#include <string>
+class URI
+{
+public:
+    static void encode(const std::string& str, std::string& encodedStr);
+    static void decode(const std::string& str, std::string& decodedStr);
+};
