000d2008522acbc548169a90763e4d9be5614113
egorlord127 <egor.lord-127@yandex.com>
2017-08-21 07:57:30 -0700

    Add XMLParser class

---
+++ libxmlWrapper/XMLParser.cpp
@@ -0,0 +1,66 @@
+#include "XMLParser.h"
+
+namespace openiam
+{
+    template <class Parser>
+    XMLParser<Parser>::XMLParser(const std::string& s)
+    : parser()
+    , doc(NULL)
+    {
+        doc = parser.parse(s);
+    }
+
+    template <class Parser>
+    XMLParser<Parser>::~XMLParser()
+    {
+        if (doc != NULL)
+            xmlFree(doc);
+    }
+
+    template <class Parser>
+    void XMLParser<Parser>::parse(std::list<XMLNode>& nodes)
+    {
+        xmlNode *current = xmlDocGetRootElement(doc);
+        while (current != NULL)
+        {
+            XMLNode result;
+            populateNode(current, result);
+            nodes.push_back(result);
+            current = current->next;
+        }
+    }
+
+    void populateNode(xmlNode *node, XMLNode& result)
+    {
+        result.name = std::string((const char*) node->name);
+        if (node->xmlChildrenNode != NULL && node->xmlChildrenNode->type == XML_TEXT_NODE)
+        {
+            result.text = std::string((char *)xmlNodeGetContent(node->xmlChildrenNode));
+        }
+
+        getAttributes(node, result.attributes);
+        getChildren(node, result);
+    }
+
+    void getAttributes(xmlNode *node, std::map<std::string, std::string>& data)
+    {
+        for (xmlAttrPtr attr = node->properties; attr != NULL; attr = attr->next)
+        {
+            data.insert(std::make_pair(std::string((const char*)attr->name), std::string((const char*)attr->children)));
+        }
+    }
+
+    void getChildren(xmlNode *node, XMLNode& result)
+    {
+        if ( node->type != XML_ELEMENT_NODE )   
+            return;
+        node = node->children;
+        while ( node != NULL ) {
+            XMLNode child;
+            populateNode(node, child);
+            result.children[child.name].push_back(child);
+            node = node->next;
+        }
+    }
+    
+}
---
+++ libxmlWrapper/XMLParser.h
@@ -0,0 +1,42 @@
+#include <string>
+#include <list>
+#include <map>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include "XMLNode.h"
+
+namespace openiam
+{
+    struct XMLFileParser
+    {
+        xmlDocPtr parse(const std::string& file)
+        {
+            return xmlParseFile(file.c_str());
+        }
+    };
+
+    struct XMLMemoryParser
+    {
+        xmlDocPtr parse(const std::string& xml)
+        {
+            return xmlParseMemory(xml.c_str(), xml.size());
+        }
+    };
+
+    template <class Parser>
+    class XMLParser 
+    {
+    public:
+        XMLParser(const std::string& s);
+        ~XMLParser();
+        void parse(std::list<XMLNode>& nodes);
+
+    private:
+        Parser parser;
+        xmlDocPtr doc;
+    };
+
+    void getAttributes(xmlNode *node, std::map<std::string, std::string>& data);
+    void getChildren(xmlNode *node, XMLNode& result);
+    void populateNode(xmlNode *node, XMLNode& result);
+}
